ce6ed7524a5af6a488b3fd194dfd29064b12a59a
Michael Rabe <m.trabe96@gmail.com>
2020-12-17 19:14:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array= ["beef", "pork"]
-  add_element_to_end_of_array.push("fish")
+  element= "fish"
+  
 end
 
 def add_element_to_start_of_array(array, element)
