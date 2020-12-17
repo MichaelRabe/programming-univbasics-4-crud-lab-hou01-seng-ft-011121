f341550a8a67d98ce3332a8c175cbe8cd9790902
Michael Rabe <m.trabe96@gmail.com>
2020-12-17 19:20:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   array= ["beef", "pork"]
-  element= "fish"
-  array.push("fish")
+  element= "arrays!"
+  array.push(element)
   end
 
 def add_element_to_start_of_array(array, element)
